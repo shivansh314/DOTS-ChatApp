0f0eefbb955b97ac1699a4a0c843b8ee40afd8fd
Shivansh Barthwal <shivanshbarthwal3.14@gmail.com>
2025-06-03 01:38:57 +0530

    add Dockerfiles and dockercompose

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.MODE === "development"
+//       ? "http://localhost:5001/api"
+//       : "/api",
+//   withCredentials: true,
+// });
+
+
+
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
+
